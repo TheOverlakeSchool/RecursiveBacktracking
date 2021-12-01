564c73f1701b1cf548a77ef0170f79102a999352
zerbash <dbasham@overlake.org>
2021-12-01 14:20:05 -0800

    initial commit

---
+++ src/Dice.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Dice {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		while (true) {
+			System.out.println("How many dice would you like to roll? (0 to exit)");
+			int count = console.nextInt();
+			if (count == 0) {
+				break;
+			}
+			System.out.println("What is your target sum?");
+			int sum = console.nextInt();
+			diceSum(count, sum);
+		}
+	}
+
+	public static void diceRolls(int count) {
+		Stack<Integer> chosen = new Stack<>();
+		diceRolls(count, chosen);
+	}
+
+	private static void diceRolls(int available, Stack<Integer> chosen) {
+		if (available == 0) {
+			System.out.println(chosen);
+		} else {
+			for (int i = 1; i <= 6; i++) {
+				chosen.push(i); // choose
+				diceRolls(available - 1, chosen); // explore
+				chosen.pop(); // unchoose
+			}
+		}
+	}
+
+	public static void diceSum(int count, int sum) {
+		Stack<Integer> chosen = new Stack<>();
+		diceSum(count, chosen, sum);
+	}
+
+	private static void diceSum(int available, Stack<Integer> chosen, int sum) {
+		if (available == 0) {
+			if (sum(chosen) == sum) {
+				System.out.println(chosen);
+			}
+		} else {
+			for (int i = 1; i <= 6; i++) {
+				chosen.push(i); // choose
+				diceSum(available - 1, chosen, sum); // explore
+				chosen.pop(); // unchoose
+			}
+		}
+	}
+
+	private static int sum(Stack<Integer> chosen) {
+		int sum = 0;
+		Stack<Integer> temp = new Stack<>();
+		while (!chosen.isEmpty()) {
+			int value = chosen.pop();
+			sum += value;
+			temp.push(value);
+		}
+
+		while(!temp.isEmpty()) {
+			chosen.push(temp.pop());
+		}
+
+		return sum;
+	}
+}
